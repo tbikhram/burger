8df6133d45ee0fa307f3582cdafa918946ad5e8a
trevor bikhram <tbikhram@trevors-MacBook-Pro.local>
2017-01-21 20:09:41 -0800

    heroku mysql linked

--- config/connection.js
+++ config/connection.js
@@ -1,21 +1,38 @@
 var mysql = require("mysql");
 
-var connection = mysql.createConnection({
-	port: 3306,
-	host: "localhost",
-	user: "root", 
-	password: "School1313",
-	database: "burgers_db"
-});
+// var connection = mysql.createConnection({
+// 	port: 3306,
+// 	host: "localhost",
+// 	user: "root", 
+// 	password: "School1313",
+// 	database: "burgers_db"
+// });
 
-// conneciton 
-connection.connect(function(err){
-	if(err) {
-		console.error("error connecting: " + err.stack);
-		return;
-	}
-	console.log("connected as id " + connection.threadId);
-});
+// // conneciton 
+// connection.connect(function(err){
+// 	if(err) {
+// 		console.error("error connecting: " + err.stack);
+// 		return;
+// 	}
+// 	console.log("connected as id " + connection.threadId);
+// });
+var connection;
+
+if(process.env.JAWSDB_URL) {
+	connection = mysql.createConnection(process.env.JAWSDB_URL);
+}eles{
+	connection = mysql.createConnection({
+		host: "localhost",
+		user: "root",
+		password: "School1313",
+		database: "burgers_db"
+	});
+};
+
+
+
+
 
 //export connection for the ORM to use
+connection.connect();
 module.exports = connection;
